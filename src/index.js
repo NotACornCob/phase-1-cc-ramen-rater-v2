355bab295bc8179eab52324d3ac6879000e1b998
Alex <alex.kleinman@gmail.com>
2024-05-22 10:02:48 -0400

    lab troubleshoot

--- src/index.js
+++ src/index.js
@@ -1,20 +1,68 @@
 // index.js
 
 // Callbacks
-const handleClick = (ramen) => {
-  // Add code
+
+
+const addSubmitListener = {
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+  body: JSON.stringify(data),
 };
 
-const addSubmitListener = () => {
-  // Add code
-}
+
+fetch("http://localhost:3000/", addSubmitListener)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+  });
 
 const displayRamens = () => {
-  // Add code
+  fetch(`http://localhost:3000/`)
+    .then((response) => response.json())
+    .then((data) => {
+      const menu = document.querySelector("#ramen-menu");
+      const ramenImages = data.map((image) => {
+        const imgElement = document.createElement("img");
+        imgElement.src = image.image;
+        imgElement.classList.add("ramenImages");
+        menu.appendChild(imgElement);
+        return imgElement;
+      });
+
+      handleClick(ramenImages);
+    });
 };
 
+const handleClick = () => {
+  const ramenImages = document.querySelectorAll('#ramen-menu img');
+  const pictureSlot = document.querySelector("#ramen-detail > img");
+  const nameSlot = document.querySelector("#ramen-detail > h2");
+  const locationSlot = document.querySelector("#ramen-detail > h3");
+  const ratingSlot = document.querySelector("#rating-display");
+  const commentSlot = document.querySelector("#comment-display");
+
+  ramenImages.forEach(image => {
+    image.addEventListener("click", () => {
+      const clickedRamen = data.find(ramen => ramen.image === image.src);
+      if (clickedRamen) {
+        nameSlot.innerText = clickedRamen.name;
+        locationSlot.innerText = clickedRamen.restaurant;
+        pictureSlot.src = clickedRamen.image;
+        commentSlot.innerText = clickedRamen.comment;
+        ratingSlot.innerText = clickedRamen.rating;
+      }
+    });
+  });
+};
+
+
 const main = () => {
-  // Invoke displayRamens here
+  displayRamens;
+  addSubmitListener;
   // Invoke addSubmitListener here
 }
 
